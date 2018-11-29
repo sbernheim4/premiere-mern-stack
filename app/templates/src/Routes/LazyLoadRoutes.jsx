11ee6a20eb07eb59909808607b8ba89a73bcfa59
sbernheim <sambernheim@gmail.com>
2018-11-29 11:44:25 -0800

    Remove unused import

--- app/templates/src/Routes/LazyLoadRoutes.jsx
+++ app/templates/src/Routes/LazyLoadRoutes.jsx
@@ -1,7 +1,7 @@
 // Export each route component here that will be lazy loaded. This means the js file for that route
 // will not be loaded until the user navigates to that route. The exported values here are imported
 // in the Routes/index.jsx
-import React, { Component } from 'react';
+import React from 'react';
 import Loadable from 'react-loadable';
 
 export const Home = Loadable({
