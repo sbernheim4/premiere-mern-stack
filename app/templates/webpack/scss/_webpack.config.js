0898b8220418dc41d950c9907d0f5c461285f8a9
sbernheim <sambernheim@gmail.com>
2019-01-04 09:07:44 -0500

    Allow env variables to be used in the client

--- app/templates/webpack/scss/_webpack.config.js
+++ app/templates/webpack/scss/_webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const glob = require('glob');
+const dotenv = require('dotenv');
 
 /* Used to generate html file from template */
 const HtmlWebpackPlugin = require('html-webpack-plugin')
@@ -15,76 +15,86 @@ const StyleLintPlugin = require("stylelint-webpack-plugin");
 
 const WebpackBar = require('webpackbar');
 
-module.exports = {
-	entry: {
-		main: "./src/index.jsx", // Entry point of where webpack should start from
-	},
-	output: {
-		// output build file to /public folder and call the file bundle.js
-		path: __dirname + "/public",
-		filename: "[name].js"
-	},
-	module: {
-		rules: [
-			// lint all jsx files and then run babel on them before bundling
-			{
-				test: /\.jsx$/,
-				exclude: /node_modules/,
-				use: ["babel-loader", "eslint-loader"],
-			},
+module.exports = () => {
 
-			// use sass-loader, css-loader, and style-loader for all scss files
-			// sass-loader - converts scss to css
-			// css-loader - allows for using import or require statements in the jsx
-			// style-loader - injects the css into the browser in a style tag
-			{
-				test: /\.scss$/,
-				use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
-			},
+	const env = dotenv.config().parsed;
+	const envKeys = object.keys(env).reduce((prev, next) => {
+		prev[`process.env.${next}`] = JSON.stringify(env[next]);
+		return prev;
+	}, {});
 
-			{
-				test: /\.css$/,
-				use: ["style-loader", "css-loader", "postcss-loader"]
+	return {
+		entry: {
+			main: "./src/index.jsx", // Entry point of where webpack should start from
+		},
+		output: {
+			// output build file to /public folder and call the file bundle.js
+			path: __dirname + "/public",
+			filename: "[name].js"
+		},
+		module: {
+			rules: [
+				// lint all jsx files and then run babel on them before bundling
+				{
+					test: /\.jsx$/,
+					exclude: /node_modules/,
+					use: ["babel-loader", "eslint-loader"],
+				},
+
+				// use sass-loader, css-loader, and style-loader for all scss files
+				// sass-loader - converts scss to css
+				// css-loader - allows for using import or require statements in the jsx
+				// style-loader - injects the css into the browser in a style tag
+				{
+					test: /\.scss$/,
+					use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
+				},
+
+				{
+					test: /\.css$/,
+					use: ["style-loader", "css-loader", "postcss-loader"]
+				}
+			]
+		},
+
+		mode: process.env.NODE_ENV || 'development',
+
+		resolve: {
+			extensions: ['*', '.js', 'jsx', '.css', '.scss', '.sass']
+		},
+
+		devServer: {
+			historyApiFallback: true,
+			contentBase: path.join(__dirname, './public'),
+			proxy: {
+				"/api": "http://localhost:3000"
 			}
+		},
+
+		plugins: [
+			new HtmlWebpackPlugin({
+				base: './public/',
+				template: 'HTMLTemplate.js',
+				dest: 'index.html',
+				inject: false,
+				title: 'Premiere Mern Stack'
+			}),
+
+			// Optimizes css by minifying it and removing comments
+			new OptimizeCssAssetsPlugin({
+				cssProcessor: nano,
+				cssProcessorOptions: {discardComments: {removeAll: true} },
+				canPrint: true
+			}),
+
+			// CSS Linter based on rules set in the .stylelintrc file
+			new StyleLintPlugin({
+				configFile: "./.stylelintrc",
+				files: "./src/scss/*.scss"
+			}),
+
+			new WebpackBar(),
+
+			new webpack.DefinePlugin(envKeys)
 		]
-	},
-
-	mode: process.env.NODE_ENV || 'development',
-
-	resolve: {
-		extensions: ['*', '.js', 'jsx', '.css', '.scss', '.sass']
-	},
-
-	devServer: {
-		historyApiFallback: true,
-		contentBase: path.join(__dirname, './public'),
-		proxy: {
-			"/api": "http://localhost:3000"
-		}
-	},
-
-	plugins: [
-		new HtmlWebpackPlugin({
-			base: './public/',
-			template: 'HTMLTemplate.js',
-			dest: 'index.html',
-			inject: false,
-			title: 'Premiere Mern Stack'
-		}),
-
-		// Optimizes css by minifying it and removing comments
-		new OptimizeCssAssetsPlugin({
-			cssProcessor: nano,
-			cssProcessorOptions: {discardComments: {removeAll: true} },
-			canPrint: true
-		}),
-
-		// CSS Linter based on rules set in the .stylelintrc file
-		new StyleLintPlugin({
-			configFile: "./.stylelintrc",
-			files: "./src/scss/*.scss"
-		}),
-
-		new WebpackBar()
-	]
-}
+	}
